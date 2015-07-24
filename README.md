c27daa66c5bffbbbc0713819afd7097527c4c8e9
Wes Johnson <swj@wesquire.ca>
2015-07-24 10:28:39 -0400

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# htmlsrv
+
+A simple development server
+
+Allows for serving static sites from the `public/` subfolder of the current working directory, and responding to requests without the html extension.
+
+* __Install__ - `npm install -g htmlsrv`
+* __Start__ - `htmlsrv`
---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var PUBLICDIR = __dirname + '/public'
+
+app.use(function (req, res, next) {
+  if (req.path.indexOf('.') === -1) {
+    var file = PUBLICDIR + req.path + '.html'
+    fs.exists(file, function (exists) {
+      if (exists) {
+        req.url += '.html'
+      }
+      next()
+    })
+  } else {
+    next()
+  }
+})
+app.use(express.static(PUBLICDIR))
+
+var PORT = 8000
+app.listen(PORT)
+console.log('Listening on ' + PORT + ' and serving ' + PUBLICDIR)
